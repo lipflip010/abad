b354cfa234f0cda97b43571d8944b340d3de412a
lipflip010 <lipflip@posteo.de>
2020-10-29 11:56:23 +0100

    using subprocess to run command

--- app.py
+++ app.py
@@ -1,14 +1,28 @@
-from flask import Flask
+from flask import Flask, render_template
+import subprocess as sp
+import os
 
 app = Flask(__name__)
 
+
+def run(command):
+    return sp.check_output(command,cwd=os.getcwd())
+
+
 @app.route('/')
 def index():
-    return 'Hello world'
+    return render_template('index.html')
+
+
+@app.route('/time')
+def time():
+    return run(['date', '+%T'])
+
 
 @app.route('/cakes')
 def cakes():
     return 'Yummy cakes!'
+
 
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0')
